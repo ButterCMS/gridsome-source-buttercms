502d9f78b3144d7ffeff9333955f6684585595d0
lauragift21 <laurandidi21@gmail.com>
2019-07-11 17:12:40 +0100

    Add butterpost fields

--- index.js
+++ index.js
@@ -19,40 +19,65 @@ class ButterSource {
     if (!options.authToken) throw new Error('ButterSource: Missing API Key');
 
     api.loadSource(async store => {
-      console.log('About to start loading data');
+      console.log('About to start loading data...');
       await this.allButterPost(store);
       await this.allButterPages(store);
       await this.allButterCollections(store);
     });
   }
 
+    /****************************************************
+    STEP ONE: Get all butter posts
+  ****************************************************/
   async allButterPost(store) {
-    const posts = store.addContentType({
-      typeName: this.createTypeName('posts')
+    const post = await this.client.post.list()
+    const  { data } = post.data;
+    const contentType = store.addContentType({
+      typeName: this.createTypeName("posts")
     });
-    const data = await this.client.post.list()
-    console.log(data.data);
-    const item = data.data;
-      posts.addNode({
-        url: item.url
-      })
+    for (const item of data) {
+      contentType.addNode({
+        fields: {
+          title: item.title,
+          url: item.url,
+          featured_image: item.featured_image,
+          slug: item.slug,
+          created: item.created,
+          published: item.published,
+          summary: item.summary,
+          seo_title: item.seo_title,
+          body: item.body,
+          meta_description: item.meta_description,
+          status: item.status,
+          author: item.author,
+          tags: item.tags,
+          categories: item.categories,
+        }
+      });
+    }
   }
 
+   /****************************************************
+    STEP TWO: Get all butter pages
+  ****************************************************/
   async allButterPages(store) {
-    const pages = store.addContentType({
+    const post = await this.client.page.retrieve('*', 'about')
+    const { data } = post;
+    console.log(data, '>>>>>>>>>>')
+    const contentType = store.addContentType({
       typeName: this.createTypeName('pages')
     });
-    const data = await this.client.page.retrieve('*', 'about')
-     console.log(data);
   }
 
+   /****************************************************
+    STEP THREE: Get all butter collections
+  ****************************************************/
   async allButterCollections(store) {
     const collection = store.addContentType({
       typeName: this.createTypeName('collection')
     });
 
     const data = await this.client.content.retrieve(['artists'])
-     console.log(data.data);
   }
 
   createTypeName(typeName = '') {
